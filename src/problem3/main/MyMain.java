7450bc6a5ef1cce3ab71d4efb85022446b29c51d
mak-g <mayank.goyal_cs18@gla.a.c.in>
2020-03-28 15:47:53 +0530

    ............comp............

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,25 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue queue = new MyPriorityQueue();
+        queue.enqueue(new Student("Mayank", 26));
+        queue.enqueue(new Student("Aditi", 27));
+        queue.enqueue(new Student("Palak", 65));
+        queue.enqueue(new Student("Suraj", 1));
+        queue.enqueue(new Student("Vishal", 10));
+        queue.enqueue(new Student("Aman", 76));
+
+        queue.show();
+        queue.dequeue();
+        queue.show();
+
+    }
 }
